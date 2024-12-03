29ce6b9a1b3dd1ff904345e533063458972c34cb
Sploucht <remycwright@gmail.com>
2024-12-03 15:03:50 -0800

    Create Engine.cpp
    Implement Engine.cpp

---
+++ Engine.cpp
@@ -0,0 +1,69 @@
+#include "Engine.h"
+
+
+void Engine::input()
+{
+	Event event;
+	while (window.pollEvent(event))
+	{
+		//Close window
+		if (event.type == Event::Closed)
+		{
+			window.close();
+		}
+		if (event.type == sf::Event::MouseButtonPressed)
+		{
+			if (event.mouseButton.button == sf::Mouse::Left)
+			{
+				//TODO : Create loop to construct ~5 particles
+				//Pass the x,y coords to particles
+			}
+			if (event.mouseButton.button == sf::Mouse::Right)
+			{
+				//TODO : whatever extra credit we want
+			}
+		}
+}
+
+void Engine::update(float dtAsSeconds)
+{
+	for (int i = 0; i < m_particles.size())
+	{
+		if (m_particles(i).getTTL() > 0.0)
+		{
+			m_particles(i).update(dtAsSeconds);
+			i++;
+		}
+		else
+		{
+			m_particles(i).erase();
+		}
+	}
+}
+
+void Engine::draw()
+{
+	m_windwow.clear();
+	for (Particle P : m_particles)	//loop through each particle
+	{
+		m_window.draw(P);	//pass each element into m_window
+		m_window.display();	//display 
+	}
+}
+
+Engine::Engine()
+{
+	m_window.create(VideoMode::getDesktopMode())
+}
+
+void Engine::run()
+{
+	Clock clock;
+	while (m_window.isOpen())
+	{
+		input();
+		update(clock.getElapsedTime());
+		draw();
+		clock.restart();
+	}
+}
