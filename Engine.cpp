0d38a45634a9a74fa7a32adc81e96ef3e7079976
avonhat101 <51030498+avonhat101@users.noreply.github.com>
2024-12-07 13:00:36 -0800

    Update Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -64,7 +64,7 @@ void Engine::run()
 	while (m_Window.isOpen())
 	{
 		input();
-		update(clock.getElapsedTime().asSeconds()cd ..);
+		update(clock.getElapsedTime().asSeconds());
 		draw();
 		clock.restart();
 	}
