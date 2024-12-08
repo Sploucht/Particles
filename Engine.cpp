1dffc2d4d8d0c99c70337c2ac49b73f18d3b61ac
avonhat101 <51030498+avonhat101@users.noreply.github.com>
2024-12-07 20:09:45 -0800

    Update Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -60,6 +60,10 @@ void Engine::draw()
 
 void Engine::run()
 {
+	cout << "Starting Particle unit tests..." << endl;
+    	Particle p(m_Window, 4, { (int)m_Window.getSize().x / 2, (int)m_Window.getSize().y / 2 });
+    	p.unitTests();
+    	cout << "Unit tests complete.  Starting engine..." << endl;
 	Clock clock;
 	while (m_Window.isOpen())
 	{
