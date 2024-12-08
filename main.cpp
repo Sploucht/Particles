1d5f215423db5de2de41554bf574883bd66fb26b
avonhat101 <51030498+avonhat101@users.noreply.github.com>
2024-12-07 20:08:36 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,10 +6,6 @@ int main()
 	Engine engine;
 	// Start the engine
 	engine.run();
-	VideoMode vm(1920, 1080);
-	RenderWindow window(vm, "Chaos Game!!", Style::Default);
-	Particle P(window, 4, Vector2i(1,1));
-	P.unitTests();
 	// Quit in the usual way when the engine is stopped
 	return 0;
 }
