2de52465486ca47deb5162db05f100ca05e86480
avonhat101 <51030498+avonhat101@users.noreply.github.com>
2024-12-07 22:25:04 -0800

    Update Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -8,7 +8,8 @@ void Engine::input()
 		if (event.type == Event::KeyPressed && event.key.code == Keyboard::Space) timeStop = !timeStop;
 		if (event.type == Event::KeyPressed && event.key.code == Keyboard::Num1 && Game == 0) Game = 1; 
 		if (event.type == Event::KeyPressed && event.key.code == Keyboard::Num2 && Game == 0) Game = 2; 
-		
+		if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape && Game == 0) event.type == Event::Closed; 
+		if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape && Game != 0) Game = 0; 
 		//Close window
 		if (event.type == Event::Closed)
 		{
