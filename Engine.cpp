4211842eb58d6f4c978167e76bfdde2a565416a7
Sploucht <remycwright@gmail.com>
2024-12-09 12:41:40 -0800

    Update Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -159,7 +159,7 @@ void Engine::run()
 	Clock clock;
 	while (m_Window.isOpen())
 	{
-		m_framerate = 1.f / clock.GetElapsedTime();	//FPS
+		m_framerate = 1.f / clock.getElapsedTime();	//FPS
 		float dt = clock.getElapsedTime().asSeconds();
 		clock.restart();
 		input();
