0bbe48596638451aaf52aee469be257ba0f34635
avonhat101 <51030498+avonhat101@users.noreply.github.com>
2024-12-09 08:08:43 -0800

    Update Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -71,13 +71,11 @@ void Engine::input()
 					original.x = event.mouseButton.x;
 					original.y = event.mouseButton.y;
 					m_particles.push_back(Particle(m_Window, rand() % 50 + 25, Vector2i(event.mouseButton.x, event.mouseButton.y)));
-					cout << original.x << endl;
 				}
 				if (event.mouseButton.button == sf::Mouse::Right)
 				{
 					m_particles[0].setSpeed(original - Vector2f(event.mouseButton.x, event.mouseButton.y));
 					timeStop = false;
-					cout << original.x - event.mouseButton.x << endl;
 				}
 				
 			}
