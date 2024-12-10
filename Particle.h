29b1078834b90dcfd5105b6c3d661d11d8341d0f
avonhat101 <51030498+avonhat101@users.noreply.github.com>
2024-12-09 23:16:50 -0800

    Update Particle.h

--- Particle.h
+++ Particle.h
@@ -3,7 +3,7 @@
 #include <SFML/Graphics.hpp>
 #define M_PI 3.1415926535897932384626433
 const float G = 1000; //Gravity
-const float TTL = 5.0; //Time To Live
+const float TTL = 3.0; //Time To Live
 const float SCALE = 0.999;
 using namespace Matrices;
 using namespace sf;
